95866a94e87fcaf8b62d6e891ea257eeb0b3a341
ZèD <imzihad@gmail.com>
2022-06-02 16:48:35 +0600

    Replaced simple texts with embeded messages
    Signed-off-by: ZèD <imzihad@gmail.com>

--- discord/client.js
+++ discord/client.js
@@ -17,21 +17,43 @@ discordClient.addListener('messageCreate', async (message) => {
         const args = message.content.slice(configs.prefix.length).split(/ +/);
         const command = args.shift().toLowerCase();
         if (command === 'ping') {
-            message.reply(`PONG! \n${message.author.username}'s ping is roughly ${Math.round(message.client.ws.ping)}ms.`);
+            // message.reply(`PONG! \n${message.author.username}'s ping is roughly ${Math.round(message.client.ws.ping)}ms.`);
+            message.reply({
+                embeds: [
+                    {
+                        title: 'PONG!',
+                        description: `${message.author.username}'s ping is roughly ${Math.round(message.client.ws.ping)}ms.`,
+                        color: 0x00ff00
+                    }
+                ]
+            })
         }
         else if (command === 'xor') {
             const question = args.join(' ');
             const query = question.split(' ').map(x => parseInt(x));
-            if (query.length !== 2) {
-                message.reply('Please enter two numbers.');
-                return;
-            }
-            if (query[ 0 ] > 1 || query[ 1 ] > 1) {
-                message.reply('Please enter two binary numbers.');
+            if (query.length !== 2 || query[ 0 ] > 1 || query[ 1 ] > 1) {
+                message.reply({
+                    embeds: [
+                        {
+                            title: 'Invalid query',
+                            description: 'Please enter a query of the form "!xor 0 0" and inputs of 0 or 1.',
+                            color: 0xff0000
+                        }
+                    ]
+                });
                 return;
             }
             const result = await xorChecker(query);
-            message.reply(`${message.author.username} asked for XOR value of: ${question} \nThe answer is: ${result.value} \nAccurate Answer: ${result.accuracy}`);
+            // message.reply(`${message.author.username} asked for XOR value of: ${question} \nThe answer is: ${result.value} \nAccurate Answer: ${result.accuracy}`);
+            message.reply({
+                embeds: [
+                    {
+                        title: `Answer is: ${result.value}`,
+                        description: `${message.author.username} asked for XOR value of: ${question} \n\nAccurate Answer: ${result.accuracy}`,
+                        color: 0x00ff00
+                    }
+                ]
+            });
         }
         else {
             message.reply('Unknown command.');
